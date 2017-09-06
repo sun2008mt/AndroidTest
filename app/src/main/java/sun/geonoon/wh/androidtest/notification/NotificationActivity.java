4d5c2009a3dc0e8b93cc711307a6420ecd735696
Marc SUN <710641245@qq.com>
2017-09-06 20:25:21 +0800

    accomplish notification

--- app/src/main/java/sun/geonoon/wh/androidtest/notification/NotificationActivity.java
+++ app/src/main/java/sun/geonoon/wh/androidtest/notification/NotificationActivity.java
@@ -2,6 +2,8 @@ package sun.geonoon.wh.androidtest.notification;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
@@ -9,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import sun.geonoon.wh.androidtest.MainActivity;
 import sun.geonoon.wh.androidtest.R;
 
 public class NotificationActivity extends AppCompatActivity {
@@ -21,13 +24,18 @@ public class NotificationActivity extends AppCompatActivity {
         findViewById(R.id.btn_send_notice).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(NotificationActivity.this, MainActivity.class);
+                //相当于延迟启动的Intent
+                PendingIntent pi = PendingIntent.getActivity(NotificationActivity.this, 0, intent, 0);
                 NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
                 Notification notification = new NotificationCompat.Builder(NotificationActivity.this)
                         .setContentTitle("This is content title")
                         .setContentText("This is content text")
+                        .setContentIntent(pi)               //点击notification会执行相应的动作
                         .setWhen(System.currentTimeMillis())
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                        .setAutoCancel(true)          //点击后取消notification
                         .build();
                 manager.notify(1, notification);
             }
