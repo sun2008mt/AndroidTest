fb9145bf138e1d44faf4c09b3f32980a742a9d01
Marc SUN <710641245@qq.com>
2017-09-06 21:25:38 +0800

    fix and update

--- app/src/main/java/sun/geonoon/wh/androidtest/camera/CameraActivity.java
+++ app/src/main/java/sun/geonoon/wh/androidtest/camera/CameraActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -72,7 +73,7 @@ public class CameraActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
             case TAKEN_PHOTO:
-                if (requestCode == RESULT_OK) {
+                if (resultCode == RESULT_OK) {
 
                     //将拍摄的照片显示出来
                     try {
@@ -86,6 +87,7 @@ public class CameraActivity extends AppCompatActivity {
                 break;
 
             default:
+                Toast.makeText(CameraActivity.this, "default activity result", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
