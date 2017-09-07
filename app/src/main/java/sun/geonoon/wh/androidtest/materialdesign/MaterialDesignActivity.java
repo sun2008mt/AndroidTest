04bd84c8e66a6c34e746e8271d2c38e9aa2520fe
Marc <710641245@qq.com>
2017-09-08 03:31:59 +0800

    replace FrameLayout with CoordinatorLayout

--- app/src/main/java/sun/geonoon/wh/androidtest/materialdesign/MaterialDesignActivity.java
+++ app/src/main/java/sun/geonoon/wh/androidtest/materialdesign/MaterialDesignActivity.java
@@ -3,6 +3,7 @@ package sun.geonoon.wh.androidtest.materialdesign;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -53,7 +54,15 @@ public class MaterialDesignActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MaterialDesignActivity.this, "FAB clicked", Toast.LENGTH_SHORT).show();
+                //第一个参数表示当前界面任意控件，Snackbar会通过这个View自动查找最外层的布局
+                Snackbar.make(view, "Data deleted", Snackbar.LENGTH_SHORT)
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(MaterialDesignActivity.this, "Data restored", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
             }
         });
     }
