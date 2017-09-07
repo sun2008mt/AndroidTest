be6e054517f282821efdb3c7c594247ce14fec7c
Marc <710641245@qq.com>
2017-09-07 11:04:44 +0800

    add xml parser test and fix

--- app/src/main/java/sun/geonoon/wh/androidtest/network/HttpActivity.java
+++ app/src/main/java/sun/geonoon/wh/androidtest/network/HttpActivity.java
@@ -2,19 +2,27 @@ package sun.geonoon.wh.androidtest.network;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import sun.geonoon.wh.androidtest.R;
 
 public class HttpActivity extends AppCompatActivity {
@@ -34,6 +42,13 @@ public class HttpActivity extends AppCompatActivity {
                 sendRequestWithHttpURLConnection();
             }
         });
+
+        findViewById(R.id.btn_send_request_okhttp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendRequestWithOkHttp();
+            }
+        });
     }
 
     private void sendRequestWithHttpURLConnection() {
@@ -45,7 +60,7 @@ public class HttpActivity extends AppCompatActivity {
                 BufferedReader reader = null;
 
                 try {
-                    URL url = new URL("http://www.baidu.com");
+                    URL url = new URL("http://www.ofo.com/");
                     connection = (HttpURLConnection) url.openConnection();
                     connection.setRequestMethod("GET");
                     connection.setConnectTimeout(8000);
@@ -82,6 +97,29 @@ public class HttpActivity extends AppCompatActivity {
         }).start();
     }
 
+    private void sendRequestWithOkHttp() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url("http://www.geonoon.com/get_data.xml")
+                        .build();
+
+                try {
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+                    String content = parseXMLWithPull(responseData);
+                    //无法直接使用TextView的setText()方法，因为这是在子线程中，而更新界面的操作必须要在UI主线程中
+                    showResponse(content);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
     private void showResponse(final String response) {
         //Android中不允许在子线程中进行UI操作
 
@@ -92,5 +130,51 @@ public class HttpActivity extends AppCompatActivity {
                 txtResponse.setText(response);
             }
         });
+    }
+
+    private String parseXMLWithPull(String xmlData) {
+        String content = "";
+
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+
+            int eventType = xmlPullParser.getEventType();
+            String id = "", name = "", version = "";
+            while (eventType != xmlPullParser.END_DOCUMENT) {
+                String nodeName = xmlPullParser.getName();
+                switch (eventType) {
+                    //开始解析某个节点
+                    case XmlPullParser.START_TAG:
+                        if ("id".equals(nodeName)) {
+                            id = xmlPullParser.nextText();
+                        } else if ("name".equals(nodeName)) {
+                            name = xmlPullParser.nextText();
+                        } else if ("version".equals(nodeName)) {
+                            version = xmlPullParser.nextText();
+                        }
+                        break;
+
+                    //完成解析某个节点
+                    case XmlPullParser.END_TAG:
+                        if ("app".equals(nodeName)) {
+                             content += "app id is " + id + ", name is " + name + ", version is " + version + ".\n";
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+                eventType = xmlPullParser.next();
+            }
+
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return content;
     }
 }
