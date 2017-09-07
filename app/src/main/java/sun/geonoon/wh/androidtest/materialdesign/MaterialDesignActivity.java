aedb9d90af18a73be1539f55ca1c751c67f0e7df
Marc <710641245@qq.com>
2017-09-08 02:41:48 +0800

    add DrawerLayout test

--- app/src/main/java/sun/geonoon/wh/androidtest/materialdesign/MaterialDesignActivity.java
+++ app/src/main/java/sun/geonoon/wh/androidtest/materialdesign/MaterialDesignActivity.java
@@ -1,12 +1,20 @@
 package sun.geonoon.wh.androidtest.materialdesign;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import sun.geonoon.wh.androidtest.R;
 
 public class MaterialDesignActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +23,43 @@ public class MaterialDesignActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //此ActionBar的具体实现是由ToolBar来完成的
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            //显示导航按钮(HomeAsUp按钮，id为android.R.id.home)
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            //设置导航按钮图标
+            actionBar.setHomeAsUpIndicator(R.mipmap.ic_launcher);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.backup:
+                Toast.makeText(this, "You clicked Backup", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.delete:
+                Toast.makeText(this, "You clicked Delete", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.settings:
+                Toast.makeText(this, "You clicked Settings", Toast.LENGTH_SHORT).show();
+                break;
+
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+
+        return true;
     }
 }
