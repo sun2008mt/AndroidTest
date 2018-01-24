863736bd32edd721cc0e67984ddb5c5fc1f62cc6
Marc <710641245@qq.com>
2018-01-24 09:52:22 +0800

    preload fonts

--- app/src/main/java/sun/geonoon/wh/androidtest/mvp/UserActivity.java
+++ app/src/main/java/sun/geonoon/wh/androidtest/mvp/UserActivity.java
@@ -1,6 +1,8 @@
 package sun.geonoon.wh.androidtest.mvp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
